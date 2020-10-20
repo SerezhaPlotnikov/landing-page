8f186c9241e6d4e2b5539731e0a1d7ea31073b65
SerezhaPlotnikov <frymeabeaver@gmail.com>
2020-10-20 20:55:43 +0300

    add sidebar menu

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import {
+  MobileIcon,
+  Nav,
+  NavBtn,
+  NavBtnLink,
+  NavContainer,
+  NavItem,
+  NavLinks,
+  NavLog,
+  NavMenu,
+} from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Nav>
+        <NavContainer>
+          <NavLog to='/'>Project Ones</NavLog>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='about'>About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='discover'>Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='services'>Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='singup'>Sing Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='/singin'>Sing In</NavBtnLink>
+          </NavBtn>
+        </NavContainer>
+      </Nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+  CloseIcon,
+  Icon,
+  SidebarContainer,
+  SidebarLink,
+  SidebarMenu,
+  SidebarRouter,
+  SidebarWrapper,
+  SideBtnWrap,
+} from './SidebarElements'
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to='about'>About</SidebarLink>
+          <SidebarLink to='discover'>Siscover</SidebarLink>
+          <SidebarLink to='services'>Services</SidebarLink>
+          <SidebarLink to='singup'>Sing Up</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRouter to='/singin'>Sing In</SidebarRouter>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  )
+}
+export default Sidebar
